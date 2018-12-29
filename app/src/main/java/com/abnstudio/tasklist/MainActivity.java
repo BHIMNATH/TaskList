ace7d944490ba109e1b219bf7f6338146a3d0c26
Aswin Bhim Nath <aswismail@gmail.com>
2018-12-29 19:28:35 +0530

    New Task Dialog

--- app/src/main/java/com/abnstudio/tasklist/MainActivity.java
+++ app/src/main/java/com/abnstudio/tasklist/MainActivity.java
@@ -1,8 +1,12 @@
 package com.abnstudio.tasklist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.todo,menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add_task :
+                AlertDialog.Builder toDoTaskBuilder = new AlertDialog.Builder(this);
+                toDoTaskBuilder.setTitle("Add Todo Task Item");
+                toDoTaskBuilder.setMessage("Describe Task ...");
+                final EditText todoET = new EditText(this);
+                toDoTaskBuilder.setView(todoET);
+                toDoTaskBuilder.setPositiveButton("Add Task", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                    }
+                })
+        }
     }
 }
