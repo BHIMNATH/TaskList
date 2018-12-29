eb8c451702dcf9895e03539b988f49a352021e54
Aswin Bhim Nath <aswismail@gmail.com>
2018-12-29 19:17:33 +0530

    ToDo List Main Activity

--- app/src/main/java/com/abnstudio/tasklist/MainActivity.java
+++ app/src/main/java/com/abnstudio/tasklist/MainActivity.java
@@ -2,6 +2,7 @@ package com.abnstudio.tasklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         updateTodoList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.todo,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
